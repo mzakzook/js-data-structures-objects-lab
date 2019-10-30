3373202b7efd54343fcade393d8867ed5d333e75
Max Zakzook <mzakzook@me.com>
2019-10-30 09:42:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+ const driver = { max: 'audi' };
+
+ function updateDriverWithKeyAndValue(driver, key, value) {
+     return Object.assign({}, driver, { [key]: value});
+ }
+
+ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+     return Object.assign(driver, {[key]: value});
+ }
+
+ function deleteFromDriverByKey(driver, key) {
+     const newObject = Object.assign({}, driver);
+     delete newObject[key];
+     return newObject;
+ }
+
+ function destructivelyDeleteFromDriverByKey(driver, key) {
+     delete driver[key];
+     return driver;
+ }
